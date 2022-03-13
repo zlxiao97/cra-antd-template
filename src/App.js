52083bd877230b7103084c32b3eb6f425a888500
zlxiao97 <zlxiao97@yahoo.com>
2022-03-13 15:38:29 +0800

    feat: configurable homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { Layout } from "antd";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Menu from "./layout/Menu";
 import "./App.css";
 import Footer from "./layout/Footer";
+import { homePath } from "./config/routes";
 
 const { Header, Content } = Layout;
 
 export default (props) => {
   return (
-    <Layout>
-      <Header>
-        <div className="logo" />
-        <Menu />
-      </Header>
-      <Content style={{ padding: "0 50px" }}>
-        <div className="site-layout-content">
-          <Outlet />
-        </div>
-      </Content>
-      <Footer />
-    </Layout>
+    <>
+      <Layout>
+        <Header>
+          <div className="logo" />
+          <Menu />
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <div className="site-layout-content">
+            <Outlet />
+          </div>
+        </Content>
+        <Footer />
+      </Layout>
+      <Navigate replace to={homePath} />
+    </>
   );
 };
