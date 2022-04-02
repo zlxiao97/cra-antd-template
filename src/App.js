374486f9c4be49b441cffd0c9864871f9d3a4c23
肖子立 <xiaozlb@digitalchina.com>
2022-04-02 18:47:59 +0800

    feat: add pageLayout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { homePath } from "@/config/routes";
 import Layout from "@/layout/Layout";
 import useHomePage from "@/hooks/useHomePage";
-import "@/theme/main.less";
+import './global.less'
 
 export default () => {
   useHomePage(homePath);
