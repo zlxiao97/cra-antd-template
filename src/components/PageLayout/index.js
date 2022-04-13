c4b6a5878f39a0e30045c2b26bfac6440f56c535
肖子立 <xiaozlb@digitalchina.com>
2022-04-13 15:02:16 +0800

    feat: default component Route props

--- src/components/PageLayout/index.js
+++ src/components/PageLayout/index.js
@@ -6,11 +6,15 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default (props) => {
   const defaultProps = {
-    className: classNames(styles.layout, props.className),
+    className: classNames({
+      [styles.layout]: true,
+      [props.className]: true,
+      [styles["dev-helper"]]: process.env.NODE_ENV === "development"
+    }),
     isDraggable: false,
     isResizable: false,
     breakpoints: { xxl: 1600, xl: 1200, lg: 992, md: 768, sm: 576, xs: 480 },
-    cols: { xxl: 12, xl: 10, lg: 8, md: 6, sm: 4, xs: 2 },
+    cols: { xxl: 12, xl: 10, lg: 8, md: 6, sm: 4, xs: 2 }
     // rowHeight: 100,
     // margin: [0, 0]
   };
