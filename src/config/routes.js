57868c9ea02e95ecfb26303fb4ef0d9c75f370e9
肖子立 <xiaozlb@digitalchina.com>
2022-05-16 10:46:34 +0800

    feat: use relative path to configure route component

--- src/config/routes.js
+++ src/config/routes.js
@@ -7,21 +7,21 @@ const routes = [
   {
     name: "Dashboard",
     path: "dashboard",
-    component: require("../pages/Dashboard").default
+    component: "pages/Dashboard"
     // hideInMenu:true
     // component: Outlet, // 如果有子路由，父路由对应组件必须包含Outlet，否则无法显示子路由，如果不传component，则默认为Outlet
     // children: [
     //   {
     //     name: "inner",
     //     path: "inner",
-    //     component: require("../pages/Dashboard/Inner").default
+    //     component: "pages/Dashboard/Inner"
     //   }
     // ]
   },
   {
     name: "Page1",
     path: "page1",
-    component: require("../pages/Page1").default
+    component: "pages/Page1"
   }
 ];
 
